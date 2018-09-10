4f7b822cfdde78c3efdd71444bf389369adb6016
Stephen Ridings <SRidings777@aol.com>
2018-09-10 01:36:36 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-// Node Dependencies
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-// Open Server
+
 var PORT = process.env.PORT || 3000;
 
 var app = express();
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(__dirname + '/public'));
-// app.use(express.static('public'));
 
-// Parse application/x-www-form-urlencoded
+app.use(express.static(__dirname + '/public'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Handlebars
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -22,8 +19,6 @@ app.set('view engine', 'handlebars');
 
 var router = require('./controllers/burgers_controller.js');
 app.use('/', router);
-
-
 
 app.listen(PORT, function() 
 {
